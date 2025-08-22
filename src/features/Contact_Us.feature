ccc59f529f6b4b544af1429bb8d9a3b9ae446515
Prathamesh Ingale <prathamesh.d.ingale@gmail.com>
2025-08-23 01:31:26 +0530

    Add 2 more e2e tests using faker

--- src/features/Contact_Us.feature
+++ src/features/Contact_Us.feature
@@ -17,10 +17,37 @@ Feature: https://www.webdriveruniversity.com/ - Contact us page
         And I switch to new window
         And I type a first name
         And I type a last name
-       # And I enter a email address
+        # And I enter a email address
         And I type a comment
         And I click on the submit button
         Then I should be presented with a unsuccessful message
 
+
+
+    Scenario: Valid contact us form submission - Using specific data
+        Given I navigate to the webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to new window
+        And I type a specific first name "Sachin"
+        And I type a specific last name "Woods"
+        And I enter a specific email address "sachin_woods@gmail.com"
+        And I type specific text "Kindly reply back as soon as possible" and a number 2 within the comment input field
+        And I click on the submit button
+        Then I should be presented with a successful contact us submission message
+
+
+    Scenario: Valid contact us form submission - Using random data
+        Given I navigate to the webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to new window
+        And I type a random first name
+        And I type a random last name
+        And I enter a random email address
+        And I type a comment
+        And I click on the submit button
+        Then I should be presented with a successful contact us submission message
+
+
+# 
 
 
--- src/step-definations/ContactUs_Steps.ts
+++ src/step-definations/ContactUs_Steps.ts
@@ -1,24 +1,25 @@
 import { When, Then } from "@cucumber/cucumber"
 import { pageFixture } from "./hooks/browserContextFixture";
 import { expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 
 When('I type a first name', async () => {
-  
+
     const firstName_textField = pageFixture.page.getByPlaceholder("First Name")
     await firstName_textField.fill("Rahul")
 
 });
 
 When('I type a last name', async () => {
-       
+
     const lastName_textField = pageFixture.page.getByPlaceholder("Last Name")
     await lastName_textField.fill("Rabada")
 
-});       
+});
 
 When('I enter a email address', async () => {
-        
+
     const email_textField = pageFixture.page.getByPlaceholder("Email Address")
     await email_textField.fill("test@test.com")
 
@@ -26,21 +27,47 @@ When('I enter a email address', async () => {
 
 
 When('I type a comment', async () => {
-         
+
     const comments_textField = pageFixture.page.getByPlaceholder("Comments")
     await comments_textField.fill("I would like to get in touch, please let me know your availability")
 
 });
 
+
 When('I click on the submit button', async () => {
 
     const submitButton = pageFixture.page.locator('input[value="SUBMIT"]');
-    await submitButton.click();      
+    await submitButton.click();
+});
+
+
+When('I type a specific first name {string}', async (firsName: string) => {
+    const firstName_textField = pageFixture.page.getByPlaceholder("First Name")
+    await firstName_textField.fill(firsName)
+});
+
+
+When('I type a specific last name {string}', async (lastName: string) => {
+    const lastName_textField = pageFixture.page.getByPlaceholder("Last Name")
+    await lastName_textField.fill(lastName)
+});
+
+
+When('I enter a specific email address {string}', async (email: string) => {
+    const email_textField = pageFixture.page.getByPlaceholder("Email Address")
+    await email_textField.fill(email)
+
+});
+
+
+When('I type specific text {string} and a number {int} within the comment input field', async (comment: string, days: number) => {
+    const comments_textField = pageFixture.page.getByPlaceholder("Comments")
+    await comments_textField.fill(`${comment} ${days}`)
 });
 
 
 Then('I should be presented with a successful contact us submission message', async () => {
-          
+
     const messageText = await pageFixture.page.getByText('Thank You for your Message!').textContent();
     expect(messageText).toEqual("Thank You for your Message!")
 
@@ -48,9 +75,38 @@ Then('I should be presented with a successful contact us submission message', as
 
 
 Then('I should be presented with a unsuccessful message', async () => {
-          
+
     const errorMessage = await (await pageFixture.page.waitForSelector("body")).textContent()
     expect(errorMessage).toMatch(/Error: (all fields are required|Invalid email address)/)
 
 });
+
+
+// Random data - faker data
+
+When('I type a random first name', async () => {
+    
+    const randomFirstName = faker.person.firstName();
+    const firstName_textField = pageFixture.page.getByPlaceholder("First Name")
+    await firstName_textField.fill(randomFirstName)
+});
+
+
+When('I type a random last name', async () =>  {
+    const randomLastName = faker.person.lastName();
+    const lastName_textField = pageFixture.page.getByPlaceholder("Last Name")
+    await lastName_textField.fill(randomLastName)
+
+});
+
+
+When('I enter a random email address', async () =>  {    
+    
+    const randomEmail = faker.internet.email();
+    const email_textField = pageFixture.page.getByPlaceholder("Email Address");
+    await email_textField.fill(randomEmail);
+    
+});
+
+
 
